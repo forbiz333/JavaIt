58bef0ae5921f3909d9b13c684f9910977a4c440
forbiz333 <forbiz333@gmail.com>
2016-12-03 00:58:59 +0300

    1

--- HW/src/homeWork14/MegaThread.java
+++ HW/src/homeWork14/MegaThread.java
@@ -4,12 +4,12 @@ package homeWork14;
  * Class printing all numbers from Task - each object has it's own thread  
  */
 public class MegaThread extends Thread {
-
+	private static final Object monic = new Object();
 	@Override
 	public void run() {
 
 		for (int i = 0, j = 1; i < 10; i++) {
-			synchronized ("") { //synchronized for one reason: to print one raw of 10 numbers by 1 thread
+			synchronized (monic) { //synchronized for one reason: to print one raw of 10 numbers by 1 thread
 				System.out.print(getName() + ": ");
 				for (int k = 0; k < 10; k++, j++) {
 					System.out.print(j + " ");
